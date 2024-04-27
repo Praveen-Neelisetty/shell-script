0869bf0ee1c932c78f326d3371f6358b6cfa6ccb
Praveen <saipraveenneelisetty27@gmail.com>
2024-04-27 13:12:53 +0530

    testing

--- parameters.sh
+++ parameters.sh
@@ -5,4 +5,4 @@ Person2=$2
 
 echo "My name is $Person1"
 echo "I am from $Person2"
-echo "$Name-$2"
+echo "$1-$2"
---
+++ read-command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter your username"
+read $username
+echo "Please enter your password"
+read $password
+echo "username - $username, password - $password"
