be3a52c48dc8e9278412c950eb020071a70cb5b0
Praveen <saipraveenneelisetty27@gmail.com>
2024-04-27 21:51:52 +0530

    installation mysql in Shell Scripting

--- 09-installation-sql-git.sh
+++ 09-installation-sql-git.sh
@@ -5,10 +5,18 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "You are not Super User"
+    exit 1
 else
     echo "You are Super User"
 fi
 
 dnf install mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL installation FAILURE..."
+else
+    echo "MySQL installation SUCCESS"
+fi
 
 echo "is shell scriting running?"
