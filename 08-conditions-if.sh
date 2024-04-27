e8311204d65a9e237733044808b126bf00565c71
Praveen <saipraveenneelisetty27@gmail.com>
2024-04-27 20:56:31 +0530

    if condition in Shell Scripting

--- 08-conditions-if.sh
+++ 08-conditions-if.sh
@@ -5,4 +5,10 @@
 read -p "Enter your Number: " NUM
 
 echo "Entered number is: $NUM" 
-#if[]
+
+if[$NUM -gt 10]
+then
+    echo "$NUM Number greater than 10"
+else
+    echo "$NUM Number lesser than 10"
+fi
