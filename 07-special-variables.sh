e19b87ce5a0e1654ad9718dd56ed6788ced50d66
Praveen <saipraveenneelisetty27@gmail.com>
2024-04-27 20:35:24 +0530

    Special charaters in Shell Scripting

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -9,5 +9,6 @@ echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
-echo "Process ID of the current shell script: $$" sleep 60 &
+echo "Process ID of the current shell script: $$" 
+sleep 60 & #now i want process ID of this script which is running in background
 echo "Process ID of last background command: $!"
