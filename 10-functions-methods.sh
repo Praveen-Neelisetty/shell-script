3e25ca192a27bfdc7425fd6af69bfa5cd8856c3f
Praveen <saipraveenneelisetty27@gmail.com>
2024-04-28 11:47:27 +0530

    Practing installations of Git and SQL

--- 10-functions-methods.sh
+++ 10-functions-methods.sh
@@ -9,22 +9,33 @@ else
     echo "You are Super User"
 fi
 
-dnf install mysql mysql-server -y
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ...SUCCESS"
+    else
+        echo "$2 ...FAILURE" 
+}
 
-if [ $? -eq 0 ]
-then
-    echo "MySQL Installation...SUCCESS"
-else
-    echo "MySQL Installation...FAILED"
-fi
+dnf install mysql mysql-server -y
+VALIDATE $? "MySQL Installation"
+
+# if [ $? -eq 0 ]
+# then
+#     echo "MySQL Installation...SUCCESS"
+# else
+#     echo "MySQL Installation...FAILED"
+# fi
 
 dnf install git -y
+VALIDATE $? "GIT Installation"
 
-if [ $? -eq 0 ]
-then
-    echo "GIT Installation...SUCCESS"
-else
-    echo "GIT Installation...FAILED"
-fi
+# if [ $? -eq 0 ]
+# then
+#     echo "GIT Installation...SUCCESS"
+# else
+#     echo "GIT Installation...FAILED"
+# fi
 
 echo "is Shell is running? YES"
