c06e9c30eecada8bb65fabacd71ba8430397023b
Praveen <saipraveenneelisetty27@gmail.com>
2024-04-28 16:14:09 +0530

    Practing installations with Functions and Colors for Git and SQL

--- 10-functions-methods.sh
+++ 10-functions-methods.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USERID=$(id -u) #getting user id
+TIMESTAMP=$(date +%F-%H-%M-%S) #getting date and time 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #getting the File/Script name
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log  #creating seperate log file for every new software installation
+R="\e[31m" #Red Colour
+G="\e[32m" #Green Colour
+N="\e[30m" #Normal Colour
 
 if [ $USERID -ne 0 ]
 then
@@ -14,13 +20,13 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then
-        echo "$2 ...FAILURE"
+        echo -e "$2 ...$R FAILURE $N"
     else
-        echo "$2 ...SUCCESS" 
+        echo "$2 ...$G SUCCESS $N" 
     fi
 }
 
-dnf install mysql mysql-server -y
+dnf install mysql mysql-server -y &>>$LOGFILE
 VALIDATE $? "MySQL Installation"
 
 # if [ $? -eq 0 ]
@@ -30,7 +36,7 @@ VALIDATE $? "MySQL Installation"
 #     echo "MySQL Installation...FAILED"
 # fi
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "GIT Installation"
 
 # if [ $? -eq 0 ]
