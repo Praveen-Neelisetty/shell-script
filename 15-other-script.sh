d4a79b5357d553fe24ca6b5073cac92fc1b15fb0
Praveen <saipraveenneelisetty27@gmail.com>
2024-05-03 20:52:58 +0530

    Learning call other scripts from your shell scripts

--- 15-other-script.sh
+++ 15-other-script.sh
@@ -2,5 +2,5 @@
 
 COURSE="DevOps from other script"
 
-echo "Variable value from Other script: $COURSE""
+echo "Variable value from Other script: $COURSE"
 echo "PID which is there in 15-other-script.sh: $$"
