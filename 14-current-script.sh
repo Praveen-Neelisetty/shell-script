6e90163f6f1e5cf93c01759952055094337a66ac
Praveen <saipraveenneelisetty27@gmail.com>
2024-05-03 21:02:13 +0530

    Learning call other scripts from your shell scripts

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,6 +5,8 @@ COURSE="DevOps from Current Script i-e 14-current-script"
 echo "Before calling other script, course: $COURSE"
 echo "PID which is there in  14-current-script.sh: $$"
 
-./15-other-script.sh
+#./15-other-script.sh
+
+source ./15-other-script.sh
 
 echo "After calling other script, course: $COURSE"
