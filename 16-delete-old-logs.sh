d6fd7f01a46fc297f2b7f03b2ac074b23a7d47d5
Praveen <saipraveenneelisetty27@gmail.com>
2024-05-04 14:31:55 +0530

    Delete Old Logs

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIRECTORY=/tmp/app_logs
+
+if [ -d SOURCE_DIRECTORY ]
+then
+    echo "$G Souce Directory Exits $N"
+else
+    echo "$R Source Directory Doesnot Exits $N"
+fi
+
+GET_FILES=echo "find $SOURCE_DIRECTORY -name "*.logs" -mtime +14"
+
