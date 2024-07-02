5d0fffd2fa2736c7d698ef7166eb8c7f7c17304b
Praveen <saipraveenneelisetty27@gmail.com>
2024-07-02 23:25:49 +0530

    Setting Disk Usuage Shell script

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+MESSAGE=""
 
+while IFS= read -r LINE
+do 
+    USAGE=$(echo df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo df -hT | grep xfs | aws -F " " '{print $NF}')
+
+    if[ USAGE -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE+=$FOLDER is more than $DISK_THRESHOLD, current usag is: $USAGE /n
+    fi
+
+done <<< $DISK_USAGE
+
+echo "Message: $MESSAGE"
 
